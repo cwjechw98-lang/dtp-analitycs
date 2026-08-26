41dbdc8413ebaf3e964a8e69985b7aa5304d9f84
Anton <katochimotor@gmail.com>
2026-08-26 23:51:32 +0600

    debug(route): логи инициализации карты

--- web/src/components/RouteTab.tsx
+++ web/src/components/RouteTab.tsx
@@ -234,9 +234,12 @@ export default function RouteTab() {
     let destroyed = false;
     (async () => {
       if (destroyed || !node) return;
+      console.log("[RouteTab] init: map creating");
       const map = L.map(node, { preferCanvas: true }).setView([56, 44], 5);
+      console.log("[RouteTab] init: map created, cluster next");
       const group = L.layerGroup().addTo(map);
       accDotsRef.current = L.markerClusterGroup({ chunkedLoading: true, maxClusterRadius: 42 }).addTo(map);
+      console.log("[RouteTab] init: cluster added");
       selRef.current = L.layerGroup().addTo(map);
       selDrawRef.current = L.layerGroup().addTo(map);
 
